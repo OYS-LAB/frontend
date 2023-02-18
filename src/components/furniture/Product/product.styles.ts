5cf0ddeb72d3df7b1cfe53e3f1f5160d92fb8f9a
O3Olivia <cherishtmwu@gmail.com>
2023-02-18 17:51:22 +0900

    udpate: 반응형 리스트 페이지 수정

--- src/components/furniture/Product/product.styles.ts
+++ src/components/furniture/Product/product.styles.ts
@@ -16,6 +16,9 @@ export const Product = styled.li`
   &:nth-last-child(1) {
     border-right: none;
   }
+  @media (max-width: 820px) {
+    width: 50%;
+  }
 `;
 export const Product_wrap = styled.dl`
   overflow: hidden;
@@ -28,12 +31,22 @@ export const Product_wrap_image = styled.dt`
 export const Product_image = styled.img`
   width: 100%;
   height: 300px;
+  @media (max-width: 820px) {
+    height: 50%;
+  }
 `;
 export const Product_footer = styled.section`
   display: flex;
   justify-content: space-between;
   width: 100%;
   height: 45px;
+  @media (max-width: 820px) {
+    height: 48px;
+  }
+  @media (max-width: 375px) {
+    font-size: 10px;
+    height: 38px;
+  }
 `;
 export const Product_description = styled.dd`
   width: 50%;
@@ -42,6 +55,9 @@ export const Product_description = styled.dd`
   strong {
     display: block;
     padding-bottom: 7px;
+  }
+  @media (max-width: 375px) {
+    padding: 7px 0 7px 8px;
   }
 `;
 export const Product_soldOut_wrap = styled.div`
@@ -53,5 +69,11 @@ export const Product_soldOut_wrap = styled.div`
     border-radius: 15px;
     ${font};
     padding: 5px 10px;
+    @media (max-width: 375px) {
+      font-size: 7px;
+    }
+  }
+  @media (max-width: 375px) {
+    padding: 8px 9px;
   }
 `;
